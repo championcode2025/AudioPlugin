7f5eb758660cab8a1fa072a4f5558e4c1537e4b8
championcode2025 <meeth.s.bafna@gmail.com>
2026-04-07 10:14:10 +0530

    Connected sliders to paramenters

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -33,7 +33,9 @@ private:
     // access the processor object that created it.
     SimpleEQAudioProcessor& audioProcessor;
 	CustomRotarySlider peakFreqSlider, peakGainSlider, peakQualitySlider, lowCutFreqSlider, highCutFreqSlider, lowCutSlopeSlider, highCutSlopeSlider;
-
+	using APVTS = juce::AudioProcessorValueTreeState;
+	using Attachment = APVTS::SliderAttachment;
+	Attachment peakFreqSliderAttachment, peakGainSliderAttachment, peakQualitySliderAttachment, lowCutFreqSliderAttachment, highCutFreqSliderAttachment, lowCutSlopeSliderAttachment, highCutSlopeSliderAttachment;
 	std::vector<juce::Component*> getComps();
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessorEditor)
