e80b7e700ac9c035751964473b8098f2b1a72f0f
championcode2025 <meeth.s.bafna@gmail.com>
2026-04-24 18:57:05 +0530

    Analyzer enablement button is implemented

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -214,10 +214,13 @@ void PathProducer::process(juce::Rectangle<float> fftBounds, double sampleRate) 
 
 void ResponseCurveComponent::timerCallback() {
 
-    auto fftBounds = getAnalysisArea().toFloat();
-    auto sampleRate = audioProcessor.getSampleRate();
-    leftPathProducer.process(fftBounds, sampleRate);
-    rightPathProducer.process(fftBounds, sampleRate);
+    if (shouldShowFFTAnalysis) {
+        auto fftBounds = getAnalysisArea().toFloat();
+        auto sampleRate = audioProcessor.getSampleRate();
+        leftPathProducer.process(fftBounds, sampleRate);
+        rightPathProducer.process(fftBounds, sampleRate);
+    }
+    
 
 
 	if (parametersChanged.compareAndSetBool(false, true)) {
@@ -316,17 +319,20 @@ void ResponseCurveComponent::paint(juce::Graphics& g)
     for (size_t i = 1; i < mags.size(); i++) {
         responseCurve.lineTo(responseArea.getX() + i, map(mags[i]));
     }
-    auto leftChannelFFTPath = leftPathProducer.getPath();
-    leftChannelFFTPath.applyTransform(AffineTransform().translation(responseArea.getX(), responseArea.getY()));
+    if (shouldShowFFTAnalysis) {
+        auto leftChannelFFTPath = leftPathProducer.getPath();
+        leftChannelFFTPath.applyTransform(AffineTransform().translation(responseArea.getX(), responseArea.getY()));
 
-    g.setColour(Colours::skyblue);
-    g.strokePath(leftChannelFFTPath, PathStrokeType(1.f));
+        g.setColour(Colours::skyblue);
+        g.strokePath(leftChannelFFTPath, PathStrokeType(1.f));
 
-    auto rightChannelFFTPath = rightPathProducer.getPath();
-    rightChannelFFTPath.applyTransform(AffineTransform().translation(responseArea.getX(), responseArea.getY()));
+        auto rightChannelFFTPath = rightPathProducer.getPath();
+        rightChannelFFTPath.applyTransform(AffineTransform().translation(responseArea.getX(), responseArea.getY()));
 
-    g.setColour(Colours::yellow);
-    g.strokePath(rightChannelFFTPath, PathStrokeType(1.f));
+        g.setColour(Colours::yellow);
+        g.strokePath(rightChannelFFTPath, PathStrokeType(1.f));
+    }
+    
 
     g.setColour(Colours::orange);
     g.drawRoundedRectangle(getRenderArea().toFloat(), 4.f, 1.f);
@@ -518,6 +524,15 @@ SimpleEQAudioProcessorEditor::SimpleEQAudioProcessorEditor(SimpleEQAudioProcesso
             comp->highCutSlopeSlider.setEnabled(!bypassed);
 
         }
+    };
+    analyzerEnabledButton.onClick = [safePtr]() {
+        if (auto* comp = safePtr.getComponent()) {
+            auto enabled = comp->analyzerEnabledButton.getToggleState();
+            comp->responseCurveComponent.toggleAnalysisEnablement(enabled);;
+
+        }
+
+
     };
     setSize (600, 450);
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -241,6 +241,10 @@ struct ResponseCurveComponent : juce::Component,
     void timerCallback() override;
 	void paint(juce::Graphics& g) override;
     void resized() override;
+
+    void toggleAnalysisEnablement(bool enabled) {
+        shouldShowFFTAnalysis = enabled;
+    }
 private:
 	SimpleEQAudioProcessor& audioProcessor;
     juce::Atomic<bool> parametersChanged{ false };
@@ -254,6 +258,7 @@ private:
     juce::Rectangle<int> getAnalysisArea();
 
     PathProducer leftPathProducer, rightPathProducer;
+    bool shouldShowFFTAnalysis = true;
 
 };
 
